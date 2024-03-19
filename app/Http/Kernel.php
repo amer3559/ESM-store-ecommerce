3b5876ca2f82443fe56bfdcb526ae8d4101bbf6e
Amer_Morsy <amer.morsy2015@gmail.com>
2024-03-19 04:10:53 +0200

    instal pakages

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -13,6 +13,14 @@ class Kernel extends HttpKernel
      *
      * @var array<int, class-string|string>
      */
+    protected $middlewareAliases = [
+        /**** OTHER MIDDLEWARE ****/
+        'localize' => \Mcamara\LaravelLocalization\Middleware\LaravelLocalizationRoutes::class,
+        'localizationRedirect' => \Mcamara\LaravelLocalization\Middleware\LaravelLocalizationRedirectFilter::class,
+        'localeSessionRedirect' => \Mcamara\LaravelLocalization\Middleware\LocaleSessionRedirect::class,
+        'localeCookieRedirect' => \Mcamara\LaravelLocalization\Middleware\LocaleCookieRedirect::class,
+        'localeViewPath' => \Mcamara\LaravelLocalization\Middleware\LaravelLocalizationViewPath::class
+    ];
     protected $middleware = [
         // \App\Http\Middleware\TrustHosts::class,
         \App\Http\Middleware\TrustProxies::class,
